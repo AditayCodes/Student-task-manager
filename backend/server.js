c3ed4a8449eb45d7c1a4f4cb6e427e298d7e27a6
Aditay Sharma <aditaysharma2001@gmail.com>
2025-12-20 01:35:37 +0530

    Fix server port for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,27 +5,28 @@ require("dotenv").config();
 
 const app = express();
 
-// Middleware
-app.use(cors());
+/* Middleware */
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-
+/* Routes */
 const taskRoutes = require("./routes/taskRoutes");
 app.use("/api/tasks", taskRoutes);
 
-
-// Test route
+/* Test Route */
 app.get("/", (req, res) => {
   res.send("Student Task Manager API running...");
 });
 
-// MongoDB connection
+/* MongoDB Connection */
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () =>
-      console.log("Server running on port 5000")
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
     );
   })
   .catch((err) => console.error(err));
